32d34db9f1f0cdc5d20ae6c077061d30d9e583e3
abdisag1 <myabdisa@gmail.com>
2022-07-24 21:26:35 +0300

    updated config file

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def predict():
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
-    LOG.info(f"The output prediction is: {prediction}")
+    LOG.info(f"Prediction: {prediction}")
     return jsonify({'prediction': prediction})
 
 
