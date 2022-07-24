687f8cede994aa812a9407c9a1de5dd6655a3a4a
abdisag1 <myabdisa@gmail.com>
2022-07-24 21:39:09 +0300

    fstring

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ LOG.setLevel(logging.INFO)
 def scale(payload):
     """Scales Payload"""
 
-    LOG.info(f"Scaling Payload: \n{payload}")
+    LOG.info("Scaling Payload: \n%s".format(payload))
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
     return scaled_adhoc_predict
@@ -60,13 +60,13 @@ def predict():
     json_payload = request.json
     LOG.info(f"JSON payload: \n{json_payload}")
     inference_payload = pd.DataFrame(json_payload)
-    LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
+    LOG.info("Inference payload DataFrame: \n%s".format(inference_payload))
     # scale the input
     scaled_payload = scale(inference_payload)
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
-    LOG.info(f"Prediction: {prediction}")
+    LOG.info("Prediction: %s".format(prediction))
     return jsonify({'prediction': prediction})
 
 
